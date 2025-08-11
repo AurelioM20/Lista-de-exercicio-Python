7f1f8b6234f55e5350c3068c3cefc5483adae177
AurelioMVR <153546989+AurelioM20@users.noreply.github.com>
2025-08-11 17:03:31 -0300

    Delete JuntandoDuasListas.py

--- JuntandoDuasListas.py
+++
@@ -1,7 +0,0 @@
-def juntar_listas(lista1, lista2):
-    return lista1 + lista2
-
-natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
-tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
-lista_concatenada = juntar_listas(natureza, tecnologia)
-print(f"Listas juntas: {lista_concatenada}")
